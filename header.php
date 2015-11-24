78419a1f913fac3cb310afa42a534d9a2ab2185a
judsonwoods <judsonwoods@college.harvard.edu>
2015-11-23 22:38:29 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,1 +1,13 @@
 <!DOCTYPE html>
+
+<html>
+  
+  <head>
+    <?php if (isset($title)): ?>
+      <title>QUANTITAS: <?= htmlspecialchars($title) ?></title>
+    <?php else: ?>
+      <title>QUANTITAS</title>
+    <?php endif ?>
+  </head>
+  
+</html>
